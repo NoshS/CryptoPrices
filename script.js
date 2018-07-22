9f3c4bb6fa9564ae4b7bfda04ac429960b439656
Nosh <nsyed100@uottawa.ca>
2018-07-22 05:06:20 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+//Uses this site for free API
+//https://min-api.cryptocompare.com/
+
+//Steps to gt API DATA
+var request = new XMLHttpRequest();
+var url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD,CAD"; //Response = {"BTC":{"USD":7438.95,"CAD":10082.23},"ETH":{"USD":463.66,"CAD":628}}
+request.responseType = "";
+request.open("GET", url, true);
+request.send();
+
+
+//Process
+request.onload = function() {
+	if (this.readyState == 4 && this.status == 200) {
+        var data = JSON.parse(this.responseText);
+        
+    	var busd = document.getElementById('BUSD');
+    	var bcad = document.getElementById('BCAD');
+    	var eusd = document.getElementById("EUSD");
+    	var ecad = document.getElementById("ECAD");
+    	busd.textContent = data['BTC']['USD'];
+    	bcad.textContent = data['BTC']['CAD'];
+    	eusd.textContent = data['BTC']['USD'];
+    	ecad.textContent = data['BTC']['USD'];
+	    
+    }
+};
